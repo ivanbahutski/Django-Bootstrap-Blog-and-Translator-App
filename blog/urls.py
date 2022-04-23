67f20052a19eac0e2c761fed706ab39a9197397c
ivanbahutski <ivanbogutski1995@gmail.com>
2022-04-23 13:22:09 +0200

    Created templates

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.HomePage.as_view(), name='homepage'),
+    path('blog', views.PostList.as_view(), name='posts_list'),
+    path('about', views.AboutView.as_view(), name='about'),
+    path('<slug:slug>', views.PostDetails.as_view(), name='post_details'),
+
+]
