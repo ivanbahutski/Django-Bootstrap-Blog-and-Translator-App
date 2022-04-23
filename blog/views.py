faa309ac0e3ff707494594162c64bf35e072290a
ivanbahutski <ivanbogutski1995@gmail.com>
2022-04-23 13:14:57 +0200

    Created PostList PostDetails views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Post
+from django.views import generic
 
-# Create your views here.
+
+class HomePage(generic.TemplateView):
+    template_name = "homepage.html"
+
+
+class AboutView(generic.TemplateView):
+    template_name = "about.html"
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-date_created')
+    template_name = "post_list.html"
+
+
+class PostDetails(generic.DetailView):
+    model = Post
+    template_name = "post_details.html"
